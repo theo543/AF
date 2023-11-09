6d0bbdf6134d6e9b53720b275b867e681d98efe4
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-09 23:39:56 +0200

    Add simpler and faster solution with BFS 0-1 to padure

--- padure_optimized.cpp
+++ padure_optimized.cpp
@@ -1,125 +1,54 @@
 #include <fstream>
-#include <cstdint>
-#include <vector>
-
-// signed 16 bits
-#define s16 int16_t
-
-struct coords {
-    s16 x;
-    s16 y;
-    bool operator==(const coords &other) {
-        return x == other.x && y == other.y;
-    }
-    bool operator!=(const coords &other) {
-        return !(operator==(other));
-    }
-};
-
-struct union_find_node {
-    coords parent;
-    s16 size;
-    char type;
-};
-
-union_find_node uf[1000][1000];
-
-// Access uf by coords struct.
-union_find_node &ufc(coords c) {
-    return uf[c.x][c.y];
-}
-
-coords union_find(coords node) {
-    // Find which set the node is in.
-    coords set = node;
-    while(ufc(set).parent != set) {
-        set = ufc(set).parent;
-    }
-    // Traverse again to compress path.
-    coords again = node;
-    while(ufc(again).parent != again) {
-        coords tmp = ufc(again).parent;
-        ufc(again).parent = set;
-        
-    }
-    return set;
-}
-
-void union_merge(coords a, coords b) {
-    a = union_find(a);
-    b = union_find(b);
-
-    if(a == b) {
-        return;
-    }
-
-    if(ufc(a).size < ufc(b).size) {
-        std::swap(a, b);
-    }
-
-    ufc(b).parent = a;
-    ufc(a).size += ufc(b).size;
-}
-
+#include <deque>
+#include <cstring>
+char types[1000][1000];
+int dist[1000][1000];
 int main() {
-    int n, m, src_x, src_y, dst_x, dst_y;
-
+    memset(dist, CHAR_MAX, sizeof(dist));
+    int n, m;
+    int src_x, src_y, dst_x, dst_y;
     {
         std::ifstream in("padure.in");
         in >> n >> m >> src_x >> src_y >> dst_x >> dst_y;
-        for(s16 x = 0; x < n; x++) {
-            for(s16 y = 0; y < n; y++) {
-                // read type
-                in >> uf[x][y].type;
-                // initialize union find
-                uf[x][y].parent = {x, y};
-                uf[x][y].size = 1;
+        src_x--; src_y--; dst_x--; dst_y--;
+        for(int x = 0; x < n; x++) {
+            for(int y = 0; y < m; y++) {
+                in >> types[x][y];
             }
         }
     }
 
-    // Merge adjacent same-type nodes.
-    for(s16 x = 0; x < n; x++) {
-        for(s16 y = 0; y < n; y++) {
-            // Up and left is enough since union-find doesn't need merge from both sides of the edges.
-            constexpr coords dirs[] = {{-1, 0}, {0, -1}};
-            for(const auto &dir : dirs) {
-                s16 x1 = x + dir.x, y1 = y + dir.y;
-                // check OOB
-                if(!(x1 > 0 && x1 < n && y1 > 0 && y1 < n)) {
-                    continue;
+    struct coord {
+        int x, y;
+    };
+    std::deque<coord> bfs;
+    bfs.push_back({src_x, src_y});
+    dist[src_x][src_y] = 0;
+    while(!bfs.empty()) {
+        coord c = bfs.front();
+        bfs.pop_front();
+        if(c.x == dst_x && c.y == dst_y) {
+            std::ofstream out("padure.out");
+            out << dist[c.x][c.y] << "\n";
+            return 0;
+        }
+        constexpr coord dirs[] = {{1, 0}, {-1, 0}, {0, 1}, {0, -1}};
+        for(const coord &d : dirs) {
+            int x1 = c.x + d.x, y1 = c.y + d.y;
+            if(x1 < 0 || x1 >= n || y1 < 0 || y1 >= m) {
+                continue;
+            }
+            bool cost = types[c.x][c.y] != types[x1][y1];
+            if(cost + dist[c.x][c.y] < dist[x1][y1]) {
+                dist[x1][y1] = cost + dist[c.x][c.y];
+                if(cost) {
+                    bfs.push_back({x1, y1});
+                } else {
+                    bfs.push_front({x1, y1});
                 }
-                // union
-                if(uf[x1][y1].type == uf[x][y].type) {
-                    union_merge({x1, y1}, {x, y});
-                }
             }
         }
     }
 
-    // Assign consecutive IDs to each set.
-    // Reuse size as "ID".
-    // Reuse type as "visited".
-    s16 ids = 0;
-    for(s16 x = 0;x < n;x++) {
-        for(s16 y = 0;y < n;y++) {
-            union_find_node &node = ufc(union_find({x, y}));
-            if(node.type != CHAR_MAX) {
-                node.type = CHAR_MAX;
-                node.size = ids;
-                ids++;
-            }
-        }
-    }
-
-    // Generate edges.
-    std::vector<std::vector<s16>> edges(ids, std::vector<s16>(0));
-    // TODO
-
-    // Perform BFS.
-    // TODO
-
-    std::ofstream out("padure.out");
-    out << "TODO\n";
-    return 0;
+    __builtin_trap();
 }
