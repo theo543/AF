980536ec7a4024e95511e51b0ad878cf0e054a27
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-11-26 17:15:31 +0200

    Finish cheapest flights
    Turns out BFS wasn't helpful due to multiple
    ways to get to a node.
    The trick with multiple copies of the graph fixes it

--- HW2/cheapest_flight_within_k_stops.cpp
+++ HW2/cheapest_flight_within_k_stops.cpp
@@ -1,5 +1,3 @@
-#include <iostream>
-
 #include "graph.cpp"
 
 using std::vector;
@@ -7,31 +5,22 @@ using std::vector;
 class Solution {
 public:
     static int findCheapestPrice(int n, const vector<vector<int>>& flights, int src_, int dst_, int k_) {
-        const uint32_t src = src_, dst = dst_, k = k_;
+        const uint src = src_, dst = dst_, k = k_;
         graph g;
-        g.grow(n);
-        for(const auto &v : flights) {
-            g.add_edge(v.at(0), v.at(1), v.at(2));
-        }
-        auto bfs = g.bfs_distances({src});
-        std::vector<bool> too_far(n, false);
-        for(const auto &b : bfs) {
-            if(b.distance > k) {
-                too_far[b.node] = true;
+        g.grow(n * (k + 2));
+        for(uint dist = 0;dist <= k;dist++) {
+            for(const auto &v : flights) {
+                g.add_edge(v.at(0) + dist * n, v.at(1) + (dist + 1) * n, v.at(2));
             }
         }
-        g = graph();
-        g.grow(n);
-        for(const auto &v : flights) {
-            if(too_far.at(v.at(0))) {
-                continue;
-            }
-            g.add_edge(v.at(0), v.at(1), v.at(2));
+        auto res = g.dijkstra(src);
+        uint result = UINT32_MAX;
+        for(uint i = 0;i <= (k+1);i++) {
+            result = std::min(result, res.distances.at(dst + i * n));
         }
-        auto dijkstra = g.dijkstra(src);
-        if(dijkstra.distances.at(dst) == UINT32_MAX) {
+        if(result == UINT32_MAX) {
             return -1;
         }
-        return dijkstra.distances.at(dst);
+        return result;
     }
 };
