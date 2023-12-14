f5016c1860e21b93b338235457cfce84ad0819f8
Theodor Negrescu <theodor.negrescu@gmail.com>
2023-12-14 15:50:26 +0200

    Fix ghizi output

--- HW3/ghizi.cpp
+++ HW3/ghizi.cpp
@@ -1,11 +1,12 @@
 #include <fstream>
+#include <algorithm>
 #include <map>
 
 #include "graph.cpp"
 
 int main() {
     std::ifstream in("ghizi.in");
-    std::map<std::pair<uint, uint>, uint> interval_guides;
+    std::map<std::pair<uint, uint>, std::vector<uint>> interval_guides;
     graph g;
     g.grow(102);
     uint n, k;
@@ -13,19 +14,32 @@ int main() {
     g.add_edge(101, 0, k);
     for(uint x = 0, a, b;x < n;x++) {
         in >> a >> b;
-        interval_guides[std::pair<uint, uint>{a, b}] = x + 1;
-        g.add_edge(a, b, 1);
+        interval_guides[std::pair<uint, uint>{a, b}].push_back(x + 1);
+    }
+    for(const auto &pair : interval_guides) {
+        const auto &interval = pair.first;
+        const auto &guides = pair.second;
+        g.add_edge(interval.first, interval.second, guides.size());
     }
     const auto flow = g.edmonds_karp(101, 100);
+    assert(flow.total == k);
     std::ofstream out("ghizi.out");
-    out << flow.total << '\n';
+    std::vector<int> guides;
     for(const auto &node : flow.edges) {
         for(const auto &edge : node) {
             if(edge.flow != 0 && edge.src != 101 && edge.dst != 101) {
                 assert(edge.flow > 0);
-                out << interval_guides.at(std::pair<uint, uint>(edge.src, edge.dst)) << ' ';
+                auto &vec = interval_guides.at(std::pair<uint, uint>(edge.src, edge.dst));
+                for(sint x = 0;x < edge.flow;x++) {
+                    guides.push_back(vec.at(x));
+                }
             }
         }
+    }
+    out << guides.size() << '\n';
+    std::sort(guides.begin(), guides.end());
+    for(const auto guide : guides) {
+        out << guide << ' ';
     }
     out << '\n';
 }
